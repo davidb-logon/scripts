ecbef3ced214345401c35c1aefcb88eadd8590a6
David Barta <davidb@log-on.com>
2024-03-19 09:50:33 +0200

    fixed template

--- 24_deploy_basic_mainframe_env.sh
+++ 24_deploy_basic_mainframe_env.sh
@@ -6,15 +6,36 @@
 main() {
     start_time=$(date +%s)
     usage
-    init_vars "logon" "cloudstack"
+    init_vars "logon" "deploy_basic_Z_env"
     parse_command_line_arguments "$@"
     start_logging
+    check_if_ubuntu_deploy_completed # Creates globals needed for the rest:
+                                     # ZONE_ID
+    delete_all_mainframe_objects
+    create_physical_network "$ZONE_ID"
+    add_traffic_type "$PHY_ID" "Guest" 
+    add_traffic_type "$PHY_ID" "Management" 
+    enable_physical_network "$PHY_ID"
+    enable_virtual_router_element "$PHY_ID"
+    create_network "$ZONE_ID" # Creates global $NETWORK_ID
+    create_pod "$ZONE_ID" "ubuntu_pod" # Creates global $POD_ID
+    create_vlan_ip_range "$POD_ID" "$NETWORK_ID"
+    create_cluster "$ZONE_ID" "$POD_ID" "ubuntu_cluster"
+    add_host "$UBUNTU_IP" "$UBUNTU_USER" "$UBUNTU_PASSWORD" "$ZONE_ID" "$POD_ID" "$CLUSTER_ID"
+    
+    add_primary_storage "$ZONE_ID" "$POD_ID" "$CLUSTER_ID" "ubuntu_primary" 
+    add_secondary_storage "$ZONE_ID" "ubuntu_secondary"
 
-    # Insert script logic here
+    enable_zone "$ZONE_ID"
     
     end_time=$(date +%s)
     elapsed_time=$((end_time - start_time))
     logMessage "The script took $elapsed_time seconds to complete."
+
+
+
+
+    # Insert script logic here
     script_ended_ok=true
 }
 
@@ -33,7 +54,9 @@ parse_command_line_arguments() {
 usage() {
 cat << EOF
 -------------------------------------------------------------------------------
-This script 
+This script deploys a meinframe kvm host into the zone created by
+script 23.
+It first deletes everything it previouslu created on the mainframe.
 -------------------------------------------------------------------------------
 EOF
 script_ended_ok=true
