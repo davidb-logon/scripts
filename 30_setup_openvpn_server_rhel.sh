08479cf1a8005eaf800034ae2c994014f3629a00
davidb-logon <davidb@log-on.com>
2024-03-26 03:36:13 -0400

    added 30 setup_openvpn_server_rhel

--- 30_setup_openvpn_server_rhel.sh
+++ 30_setup_openvpn_server_rhel.sh
@@ -2,29 +2,42 @@
 #------------------------------------------------------------------------------
 # Licensed Materials (c) Copyright Log-On 2024, All Rights Reserved.
 #------------------------------------------------------------------------------
+mount_repo() {
+    logMessage "--- Start mounting repo"
+    case "$LINUX_DISTRIBUTION" in
+        "UBUNTU")
+            logMessage "--- No need to mount Ubuntu repository"
+            ;;
+        "RHEL")
+            logMessage "--- Mounting RHEL repository"
+            do_cmd "sudo mkdir -p /mnt/iso"  
+            do_cmd "sudo mkdir -p /mnt/linuxu"
+            do_cmd "sudo mount -t nfs 54.227.191.101:/iso /mnt/iso" "Mounted" "INFO: Already Mounted"
+            do_cmd "sudo mount -t nfs 54.227.191.101:/linuxu /mnt/linuxu" "Mounted" "INFO: Already Mounted"
+            do_cmd "sudo mount -o loop /mnt/iso/rhel/rhel-baseos-9.1-s390x-dvd.iso /mnt/rhel91/BaseOs" "Mounted" "INFO: Already Mounted"
+            ;;
+        *)
+            logMessage "ERROR:--- Unsupported LINUX_DISTRIBUTION: $LINUX_DISTRIBUTION, exiting"
+            exit 1
+            ;;
+    esac
+    logMessage "--- End mounting repo"
+}
 
 main() {
     start_time=$(date +%s)
     usage
-    init_vars "logon" "setup_openvpn"
+    init_utils_vars "logon" "setup_openvpn"
     start_logging
-
-    parse_command_line_arguments "$@" # Get the clients,
-    generate_certifiate_for_client  sefiw
-}    
-
-main2() {
-    start_time=$(date +%s)
-    usage
-    init_vars "logon" "setup_openvpn"
-    start_logging
-
-    parse_command_line_arguments "$@" # Get the clients,
+    parse_command_line_arguments "$@" # Get the clients
+    detect_linux_distribution # Sets a global: $LINUX_DISTRIBUTION
+    exit_if_unsupported_distribution
+    init_vars # Sets up linux specific variables according to distribution
+    mount_repo # Mount the repository
     install_openvpn_and_easy_rsa
     setup_CA_certificate
     generate_server_certificate_and_key
     generate_Diffie_Hellman_key_and_HMAC_signature
- 
 
     do_cmd "sudo mkdir /etc/openvpn/ccd"
 
@@ -35,7 +48,6 @@ main2() {
     do_cmd "sudo sed -i 's/;user nobody/user nobody/' /etc/openvpn/server.conf"
     do_cmd "sudo sed -i 's/;group nogroup/group nogroup/' /etc/openvpn/server.conf"
     do_cmd "sudo sed -i 's/;client-to-client/client-to-client/' /etc/openvpn/server.conf"
-
 
     # To address the issues seen in "sudo journalctl -u openvpn@server", add the following to server.conf:
     # topology subnet
@@ -50,7 +62,6 @@ main2() {
     do_cmd "sudo systemctl enable openvpn@server"
 
     logMessage "OpenVPN setup is complete. Review the configuration and adjust firewall settings accordingly."
-
     
     end_time=$(date +%s)
     elapsed_time=$((end_time - start_time))
@@ -59,26 +70,55 @@ main2() {
 }
 
 init_vars() {
-    init_utils_vars $1 $2
-}
+    CA_DIR="/home/davidb/openvpn-ca"
+    case "$LINUX_DISTRIBUTION" in
+        "UBUNTU")
+            INSTALL_CMD="apt"
+            ;;
+        "RHEL")
+            INSTALL_CMD="yum"
+            ;;        
+        *)
+            logMessage "Unknown or unsupported LINUX_DISTRIBUTION: $LINUX_DISTRIBUTION, exiting"
+            exit 1
+            ;;
+    esac
+}    
 
 install_openvpn_and_easy_rsa() {
     logMessage "--- Start installing openvpn and easy-rsa"
-    do_cmd "sudo apt update && sudo apt upgrade -y"
-    do_cmd "sudo apt install openvpn easy-rsa -y"
+    do_cmd "sudo $INSTALL_CMD update -y && sudo $INSTALL_CMD upgrade -y"
+    do_cmd "sudo $INSTALL_CMD install openvpn easy-rsa -y"
     logMessage "--- End installing openvpn and easy-rsa"
 }
 
-setup_CA_certificate() {
-    logMessage "--- Start setting up the CA certificate"
-    # Set up Easy-RSA directory
-    do_cmd "make-cadir ~/openvpn-ca" "Created cadir" "INFO: Unable to create ca_dir"
-    cd ~/openvpn-ca
+setup_easyrsa_dir() {
+    logMessage "--- Start setting up Easy RSA directory"
+    case "$LINUX_DISTRIBUTION" in
+        "UBUNTU")
+            do_cmd "sudo make-cadir $CA_DIR" "Created $CA_DIR" "INFO:Unable to create $CA_DIR"
+            ;;
+        "RHEL")
+            do_cmd "sudo mkdir -p $CA_DIR"
+            do_cmd "sudo cp -r /usr/share/easy-rsa/3.1.6/* $CA_DIR/"
+            ;;        
+        *)
+            logMessage "Unknown or unsupported LINUX_DISTRIBUTION: $LINUX_DISTRIBUTION, exiting"
+            exit 1
+            ;;
+    esac
+    cd "$CA_DIR"
     init_RSA_vars
+    logMessage "--- End setting up Easy RSA directory"
+}
 
+setup_CA_certificate() {
+    logMessage "--- Start setting up the CA certificate at: $(pwd)"
+    setup_easyrsa_dir
+    logMessage "--- Current dir is: $(pwd)"
     # Initialize and build CA
-    do_cmd "./easyrsa init-pki"
-    do_cmd "echo 'CA' | ./easyrsa build-ca nopass"
+    do_cmd "sudo ./easyrsa init-pki --vars=vars"
+    do_cmd "echo 'CA' | sudo ./easyrsa build-ca nopass"
 
     # Copy the CA certificate to the OpenVPN directory
     do_cmd "sudo cp pki/ca.crt /etc/openvpn/"
@@ -88,7 +128,7 @@ setup_CA_certificate() {
 init_RSA_vars() {
     # Configuring Easy-RSA vars
     # Update these vars according to your organization's details.
-cat > vars <<EOF
+cat <<EOF | sudo tee pki/vars
 set_var EASYRSA_REQ_COUNTRY    "IL"
 set_var EASYRSA_REQ_PROVINCE   "Galillee"
 set_var EASYRSA_REQ_CITY       "Kfar Vradim"
@@ -104,16 +144,18 @@ set_var EASYRSA_NS_COMMENT     "OpenVPN Certificate"
 set_var EASYRSA_EXT_DIR        "\${EASYRSA}/x509-types"
 set_var EASYRSA_SSL_CONF       "\${EASYRSA}/openssl-easyrsa.cnf"
 set_var EASYRSA_DIGEST         "sha256"
-EOF    
+EOF
 }
+
+
 
 generate_server_certificate_and_key() {
     logMessage "--- Start generating server certificate and key"
     # Generate server certificate and key
-    do_cmd "./easyrsa gen-req server nopass"
+    do_cmd "sudo ./easyrsa gen-req server nopass"
     do_cmd "sudo cp pki/private/server.key /etc/openvpn/"
 
-./easyrsa sign-req server server <<EOF
+sudo ./easyrsa sign-req server server <<EOF
 yes
 EOF
 
@@ -122,13 +164,12 @@ EOF
     logMessage "--- End generating server certificate and key, at: /etc/openvpn/server.crt"
 }
 
-
 generate_Diffie_Hellman_key_and_HMAC_signature() {
     logMessage "--- Start generating Diffie Hellman key and HMAC signature"
     
     # Generate Diffie-Hellman key and HMAC signature
-    do_cmd "./easyrsa gen-dh"
-    do_cmd "openvpn --genkey --secret ta.key"
+    do_cmd "sudo ./easyrsa gen-dh"
+    do_cmd "sudo openvpn --genkey --secret ta.key"
 
     # Move them to the OpenVPN directory
     do_cmd "sudo cp ta.key /etc/openvpn/"
@@ -141,7 +182,7 @@ parse_command_line_arguments() {
     CLIENTS=("$@") # Get them as an array
     NUM_CLIENTS=${#clients[@]}
 
-    if [ NUM_CLIENTS -eq 0 ]; then
+    if [[ NUM_CLIENTS -eq 0 ]]; then
         message="No clients names provided on command line, none will be assigned fixed ips."
         logMessage $message
         confirm "${message} Continue?" || exit 1
@@ -163,33 +204,7 @@ EOF
 script_ended_ok=true
 }
 
-generate_certifiate_for_client() {
-    # This function in this snippet is a shell script function that generates a certificate for a client
-    # in an OpenVPN setup. It sets up the necessary directories, copies the required files, and updates 
-    # the OpenVPN configuration file with client-specific details before creating a zip file containing the 
-    # client's configuration.
-    client="$1"
-    vpnserver="84.95.45.250"
-    srcdir="/home/davidb/openvpn-ca"
-    logMessage "--- Start generating certificate for client: $client"
-    cd /home/davidb/openvpn-ca
-    do_cmd "path_easyrsa=$(sudo find /usr/share/easy-rsa/ | grep easyrsa | grep -v cnf)"
-    do_cmd "sudo $path_easyrsa gen-req $client nopass"
-    do_cmd "mkdir -p ~/ovpn-$client"
-    do_cmd "sudo cp ${srcdir}/pki/private/$client.key ~/ovpn-$client"
-    do_cmd "sudo cp ${srcdir}/pki/issued/$client.crt ~/ovpn-$client"
-    do_cmd "sudo cp ${srcdir}/ta.key ~/ovpn-$client/."
-    do_cmd "sudo cp ${srcdir}/pki/ca.crt ~/ovpn-$client/."
-    do_cmd "sudo cp /usr/share/doc/openvpn/examples/sample-config-files/client.conf ~/ovpn-$client/$client.ovpn"
-    cd ~/ovpn-$client
-    do_cmd "sudo chown $USER:$USER *"
-    do_cmd "sed -i 's/cert client.crt/cert $client.crt/g' $client.ovpn"
-    do_cmd "sed -i 's/key client.key/key $client.key/g' $client.ovpn"
-    do_cmd "sed -i 's/remote my-server-1/remote $vpnserver/g' $client.ovpn"
-    do_cmd "zip $client.ovpn.zip $client.ovpn $client.key $client.crt ca.crt ta.key"
-}
-
-setup_ovpn_client_as_service(){
+function setup_ovpn_client_as_service(){
 #    
 #/etc/systemd/system/openvpn@client.service (replace client with your specific client name if needed). Use a text editor like nano to edit the file.
 #
@@ -227,4 +242,4 @@ source "$DIR/lib/common.sh"
 script_ended_ok=false
 trap 'cleanup' EXIT
 
-main2 "$@"
+main "$@"
