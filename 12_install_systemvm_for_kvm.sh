8da601e69bb0dca06b24bb99e898f6958547167b
David Barta <david@odeca.net>
2024-07-30 16:01:04 +0300

    adjust install systemvm for mainframe

--- 12_install_systemvm_for_kvm.sh
+++ 12_install_systemvm_for_kvm.sh
@@ -2,4 +2,8 @@
 #------------------------------------------------------------------------------
 # Licensed Materials (c) Copyright Log-On 2024, All Rights Reserved.
 #------------------------------------------------------------------------------
-sudo /usr/share/cloudstack-common/scripts/storage/secondary/cloud-install-sys-tmplt -m /data/ubuntu_secondary -u http://download.cloudstack.org/systemvm/4.19/systemvmtemplate-4.19.0-kvm.qcow2.bz2 -h kvm -F
+
+SCRIPT_PATH="/usr/share/cloudstack-common/scripts/storage/secondary/cloud-install-sys-tmplt"
+SVM_PATH=http://download.cloudstack.org/systemvm/4.19/systemvmtemplate-4.19.1-kvm.qcow2.bz2
+
+sudo $SCRIPT_PATH -m /data/mainframe_secondary -u $SVM_PATH -h kvm -F
