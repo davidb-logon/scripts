8a2c2240a755b6c27ec3aa3d0dd6dbec38aff0f7
David Barta <david@odeca.net>
2024-07-30 11:10:46 +0300

    update web server on repo

--- util/start_web_server_on_repo.sh
+++ util/start_web_server_on_repo.sh
@@ -11,9 +11,14 @@ init_utils_vars
 
 port=${1:-8090} # You can provide a port via $1, or it will use the default 8090
 
-PACKAGES_OUTPUT_DIR="/home/davidb/logon/packages"
-DEBIAN_PACKAGES_OUTPUT_DIR="$PACKAGES_OUTPUT_DIR/debian"
-cd "$DEBIAN_PACKAGES_OUTPUT_DIR"
+# PACKAGES_OUTPUT_DIR="/home/davidb/logon/packages"
+# DEBIAN_PACKAGES_OUTPUT_DIR="$PACKAGES_OUTPUT_DIR/debian"
+
+#DEBIAN_PACKAGES_OUTPUT_DIR="$PACKAGES_OUTPUT_DIR/debian"
+RPM_PACKAGES_OUTPUT_DIR="/data/repo"
+
+#cd "$DEBIAN_PACKAGES_OUTPUT_DIR"
+cd "$RPM_PACKAGES_OUTPUT_DIR"
 
 if ! ps aux | grep "python3 -m http.server" | grep -v grep; then
     #python3 -m http.server $port &
