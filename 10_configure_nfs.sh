bcba829aee3486884bb8aa775e4d4f22de6ee307
David Barta <david@odeca.net>
2024-07-30 15:54:41 +0300

    adjust configure_nfs for mainframe

--- 10_configure_nfs.sh
+++ 10_configure_nfs.sh
@@ -12,56 +12,11 @@ source "$DIR/lib/nfsconfig.sh"
 script_ended_ok=false
 trap 'cleanup' EXIT
 
-usage() {
-cat << EOF
--------------------------------------------------------------------------------
-Install Cloudstack server from local repo 
-
-Following instructions at:
-http://docs.cloudstack.apache.org/en/4.19.0.0/installguide/management-server/index.html
-
-
--------------------------------------------------------------------------------
-EOF
-script_ended_ok=true
-}
-
-prepare_nfs_shares() {
-    # See: http://docs.cloudstack.apache.org/en/4.19.0.0/installguide/management-server/index.html#using-the-management-server-as-the-nfs-server
-
-    logMessage "--- Start to prepare NFS shares"
-    do_cmd "$SUDO $CMD install $NFS_SERVER"
-    do_cmd "$SUDO mkdir -p /data/$PRIMARY"
-    do_cmd "$SUDO mkdir -p /data/$SECONDARY"
-    
-    set_nfs_exports_options "/data/$PRIMARY" "/data/$SECONDARY"
-    if [[ $LINUX_DISTRIBUTION = "UBUNTU" ]]; then
-        configure_nfs_ports_on_ubuntu
-    fi
-    
-    logMessage "--- End of preparing NFS shares"
-}
-
-prepare_os() {
-    if [[ $LINUX_DISTRIBUTION = "RHEL" ]]; then
-        do_cmd "mount_iso_for_mainframe.sh"
-    fi
-}
-
-mount_nfs() {
-    logMessage "--- Starting to mount nfs"
-    do_cmd "sudo mkdir -p /mnt/$PRIMARY"
-    do_cmd "sudo mkdir -p /mnt/$SECONDARY"
-    do_cmd "sudo mount -t nfs localhost:/data/$PRIMARY /mnt/$PRIMARY" "/mnt/$PRIMARY was mounted."
-    do_cmd "sudo mount -t nfs localhost:/data/$SECONDARY /mnt/$SECONDARY" "/mnt/$SECONDARY was mounted."
-    logMessage "--- End mount nfs"
-}
-
 main() {
-    # Replace logon and template with your own values
     detect_linux_distribution # Sets global variable $LINUX_DISTRIBUTION
     init_vars "logon" "configure_nfs"
     start_logging
+    check_if_root
     prepare_os
     prepare_nfs_shares
     mount_nfs
@@ -76,14 +31,12 @@ init_vars() {
         NFS_SERVER="nfs-kernel-server"
         PRIMARY="ubuntu_primary"
         SECONDARY="ubuntu_secondary"
-        SUDO="sudo"
       ;;
     "RHEL")
         CMD="yum"
         NFS_SERVER="nfs-utils"
         PRIMARY="mainframe_primary"
         SECONDARY="mainframe_secondary"
-        SUDO="sudo"
       ;;
     *)
       logMessage "Unknown or Unsupported LINUX_DISTRIBUTION: $LINUX_DISTRIBUTION, exiting"
@@ -92,5 +45,58 @@ init_vars() {
     esac    
 
 }
+ 
+usage() {
+cat << EOF
+-------------------------------------------------------------------------------
+Install Cloudstack server from local repo 
+
+Following instructions at:
+http://docs.cloudstack.apache.org/en/4.19.0.0/installguide/management-server/index.html
+
+
+-------------------------------------------------------------------------------
+EOF
+script_ended_ok=true
+}
+
+prepare_os() {
+    do_cmd "$CMD install $NFS_SERVER"
+    do_cmd "systemctl enable $NFS_SERVER"
+    do_cmd "systemctl restart $NFS_SERVER"
+    if [[ $LINUX_DISTRIBUTION = "RHEL" ]]; then
+        do_cmd "mount_iso_for_mainframe.sh"
+    fi
+}
+
+
+prepare_nfs_shares() {
+    # See: http://docs.cloudstack.apache.org/en/4.19.0.0/installguide/management-server/index.html#using-the-management-server-as-the-nfs-server
+
+    logMessage "--- Start to prepare NFS shares"
+    do_cmd "mkdir -p /data/$PRIMARY"
+    do_cmd "mkdir -p /data/$SECONDARY"
+    do_cmd "chmod 777 /data/$PRIMARY"
+    do_cmd "chmod 777 /data/$SECONDARY"
     
+    set_nfs_exports_options "/data/$PRIMARY" "/data/$SECONDARY"
+    if [[ $LINUX_DISTRIBUTION = "UBUNTU" ]]; then
+        configure_nfs_ports_on_ubuntu
+    fi
+    logMessage "--- End of preparing NFS shares"
+}
+
+
+mount_nfs() {
+    logMessage "--- Starting to mount nfs"
+    do_cmd "mkdir -p /mnt/$PRIMARY"
+    do_cmd "mkdir -p /mnt/$SECONDARY"
+    do_cmd "chmod 777 /mnt/$PRIMARY"
+    do_cmd "chmod 777 /mnt/$SECONDARY"
+    do_cmd "mount -t nfs localhost:/data/$PRIMARY /mnt/$PRIMARY" "/mnt/$PRIMARY was mounted."
+    do_cmd "mount -t nfs localhost:/data/$SECONDARY /mnt/$SECONDARY" "/mnt/$SECONDARY was mounted."
+    logMessage "--- End mount nfs"
+}
+
+   
 main "$@"
