e7dc55fbb4397540d3679e770de0bcebd03f2b2f
sefimlog <sefim@log-on.com>
2024-07-31 13:21:04 +0300

    fwovpn.sh

--- fwovpn.sh
+++ fwovpn.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
+
 # Flush existing rules
-sudo iptables -F
-sudo iptables -t nat -F
-sudo iptables -t mangle -F
-sudo iptables -X
-sudo iptables -t nat -X
-sudo iptables -t mangle -X
+iptables -F
+iptables -t nat -F
+iptables -t mangle -F
+iptables -X
+iptables -t nat -X
+iptables -t mangle -X
 
 # Enable IP forwarding
-sudo sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv4.ip_forward=1
 
 # Add forwarding rules
-sudo iptables -A FORWARD -i tun0 -o virbr0 -j ACCEPT
-sudo iptables -A FORWARD -i virbr0 -o tun0 -j ACCEPT
+iptables -A FORWARD -i tun0 -o virbr0 -j ACCEPT
+iptables -A FORWARD -i virbr0 -o tun0 -j ACCEPT
 
 # Add NAT rule
-sudo iptables -t nat -A POSTROUTING -s 192.168.122.0/24 -o tun0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.122.0/24 -o tun0 -j MASQUERADE
 
 # Ensure VPN client traffic can reach `virsh` network
-sudo iptables -A FORWARD -i tun0 -s 192.168.123.0/24 -d 192.168.122.0/24 -j ACCEPT
-sudo iptables -A FORWARD -i virbr0 -s 192.168.122.0/24 -d 192.168.123.0/24 -j ACCEPT
+iptables -A FORWARD -i tun0 -s 192.168.123.0/24 -d 192.168.122.0/24 -j ACCEPT
+iptables -A FORWARD -i virbr0 -s 192.168.122.0/24 -d 192.168.123.0/24 -j ACCEPT
