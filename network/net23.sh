b83390bee5535da51067031e895a564b7d7a801e
sefimlog <sefim@log-on.com>
2024-08-08 12:32:45 +0300

    net23 update

--- network/net23.sh
+++ network/net23.sh
@@ -1,4 +1,10 @@
-sudo nmcli connection del eth0
+sudo ip link add eth0 type dummy
+
+sudo ip add add 192.168.122.1/24 dev cloudbr0 brd 192.168.122.255 
+#ip route add default via 204.90.115.1 dev cloudbr0
+sudo ip link set eth0 up
+
+#sudo nmcli connection del eth0
 nmcli connection show
 nmcli con delete cloudbr0
 sudo nmcli connection add type bridge autoconnect yes con-name cloudbr0 ifname cloudbr0
