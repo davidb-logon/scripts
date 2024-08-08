35b941c0c6f30ff800b511c3c3a5fc82abf3b9c0
sefimlog <sefim@log-on.com>
2024-08-08 17:59:40 +0300

    added 41 using ip only

---
+++ 41_ip_set_cloudbr0_cloudbr1_on_rhel_z.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#------------------------------------------------------------------------------
+# Licensed Materials (c) Copyright Log-On 2024, All Rights Reserved.
+#------------------------------------------------------------------------------
+# See usage for what this script does.
+# TODOs:
+
+# Source script libraries as needed.
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "$DIR/lib/common.sh"
+
+script_ended_ok=false
+trap 'cleanup' EXIT
+
+usage() {
+cat << EOF
+-------------------------------------------------------------------------------
+Setup networking on KVM host on RHEL 8.6 on Z, following instructions at:
+https://docs.cloudstack.apache.org/en/4.19.1.0/installguide/hypervisor/kvm.html#configuring-the-networking
+
+-------------------------------------------------------------------------------
+EOF
+script_ended_ok=true
+}
+
+main() {
+    init_vars "logon" "set_cloudbr0_on_rhel_z"
+    start_logging
+    check_if_root
+
+    remove_existing_connections
+    create_eth0
+    create_cloudbr0
+    attach_eth0_to_cloudbr0
+    
+    ip -br link show
+    ip -br addr show
+    
+    script_ended_ok=true
+}
+
+init_vars() {
+    init_utils_vars $1 $2
+}
+
+remove_existing_connections() {
+    logMessage "Removing existing network connections if they exist."
+
+    do_cmd "ip link set eth0 down || true"
+    do_cmd "ip link set cloudbr0 down || true"
+    do_cmd "ip link delete eth0 || true"
+    do_cmd "ip link delete cloudbr0 || true"
+    do_cmd "ip link delete cloud0 || true"
+    do_cmd "ip link delete cloudbr1 || true"
+    do_cmd "ip link delete eth0.200 || true"
+    do_cmd "rm -f /etc/sysconfig/network-scripts/ifcfg-cloudbr0*"
+    do_cmd "rm -f /etc/sysconfig/network-scripts/route-cloudbr0*"
+}
+
+create_eth0() {
+    logMessage "Creating eth0 interface."
+    do_cmd "ip link add name eth0 type dummy"
+    do_cmd "ip link set dev eth0 up"
+}
+
+create_cloudbr0() {
+    logMessage "Creating cloudbr0 bridge."
+    do_cmd "ip link add name cloudbr0 type bridge"
+    do_cmd "ip addr add 192.168.122.1/24 dev cloudbr0"
+    do_cmd "ip link set dev cloudbr0 up"
+    do_cmd "ip route add default via 204.90.115.1 dev cloudbr0"
+}
+
+attach_eth0_to_cloudbr0() {
+    logMessage "Attaching eth0 to cloudbr0."
+    do_cmd "ip link set dev eth0 master cloudbr0"
+    do_cmd "ip link set dev eth0 up"
+}
+
+main "$@"
