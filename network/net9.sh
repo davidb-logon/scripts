c4313e0d3cd7209bb445bd18345dc1a7bc1b7a06
sefimlog <sefim@log-on.com>
2024-08-08 04:18:07 -0500

    net9.sh

--- network/net9.sh
+++ network/net9.sh
@@ -1,5 +1,5 @@
-ifconfig eth0 204.90.115.226 netmask 255.255.255.0  up
-ip route add default via 204.90.115.1 dev eth0 proto static metric 100      
+ifconfig enc100 204.90.115.226 netmask 255.255.255.0  up
+ip route add default via 204.90.115.1 dev enc100 proto static metric 100      
 ip a
 ip route
 
