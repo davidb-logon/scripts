ab7126af643057efe98d133bcd1a51ac65436722
dbarta <david@odeca.net>
2024-08-11 16:14:28 +0300

    12 to run the script

--- 12_install_systemvm_for_kvm.sh
+++ 12_install_systemvm_for_kvm.sh
@@ -2,11 +2,12 @@
 #------------------------------------------------------------------------------
 # Licensed Materials (c) Copyright Log-On 2024, All Rights Reserved.
 #------------------------------------------------------------------------------
-
+set -x
 SCRIPT_PATH="/usr/share/cloudstack-common/scripts/storage/secondary/cloud-install-sys-tmplt"
 SVM_PATH=http://download.cloudstack.org/systemvm/4.19/systemvmtemplate-4.19.1-kvm.qcow2.bz2
 
-#sudo $SCRIPT_PATH -m /data/mainframe_secondary -u $SVM_PATH -h kvm -F
+sudo $SCRIPT_PATH -m /data/mainframe_secondary -u $SVM_PATH -h kvm -F
+exit
 
 mkdir -p /data/test_qemu
 cd /data/test_qemu
