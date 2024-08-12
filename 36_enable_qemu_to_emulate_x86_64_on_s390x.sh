2c5072890d776ffca0c2cebe7c3fa3a79df68838
dbarta <david@odeca.net>
2024-08-12 10:02:49 +0300

    fixed library path

--- 36_enable_qemu_to_emulate_x86_64_on_s390x.sh
+++ 36_enable_qemu_to_emulate_x86_64_on_s390x.sh
@@ -110,7 +110,8 @@ install_glib2() {
     do_cmd "meson setup .. --prefix=/usr/local"
     do_cmd "ninja"
     do_cmd "ninja install"
-    do_cmd "export PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig"
+    #do_cmd "export PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig"
+    do_cmd "export PKG_CONFIG_PATH=/usr/local/glib-2.66.8/lib64/pkgconfig"
     # Update library cache
     do_cmd "ldconfig"
 
