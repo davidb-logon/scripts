24fe44730fe02cd45225222bb011c64adbdb9f05
dbarta <david@odeca.net>
2024-08-13 12:51:23 +0300

    added 43 to net9

--- network/net9.sh
+++ network/net9.sh
@@ -4,4 +4,7 @@ ip route add default via 204.90.115.1 dev enc1c00 proto static metric 100
 echo -e 'nameserver 8.8.8.8\nnameserver 8.8.4.4' >> /etc/resolv.conf  
 ip a
 ip route
+# Add cloudbr0 and eth0
+/data/scripts/43_configure_network_without_nm.sh 
+
 
