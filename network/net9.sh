eec37acab0b2b0cb2ccdad072e3fe1143a0b7e8b
sefimlog <sefim@log-on.com>
2024-08-13 11:50:44 +0300

    small fix to net9 and 41

--- network/net9.sh
+++ network/net9.sh
@@ -1,6 +1,7 @@
 
 ifconfig enc1c00 204.90.115.226 netmask 255.255.255.0  up
-ip route add default via 204.90.115.1 dev enc1c00 proto static metric 100      
+ip route add default via 204.90.115.1 dev enc1c00 proto static metric 100    
+echo -e 'nameserver 8.8.8.8\nnameserver 8.8.4.4' >> /etc/resolv.conf  
 ip a
 ip route
 
