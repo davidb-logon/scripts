1d0c5ef5250c998708e45d0600011e3979b9105d
dbarta <david@odeca.net>
2024-08-14 17:03:49 +0300

    החזר ימינו כקדם

--- groovy/transformer.groovy
+++ groovy/transformer.groovy
@@ -2,6 +2,9 @@ import groovy.xml.*
 import java.text.SimpleDateFormat
 import java.util.Date
 
+// Define the log file path
+
+
 def writeLog(String msg) {
     // Get the current date and time
     def currentDate = new Date()
@@ -17,16 +20,40 @@ def writeLog(String msg) {
     println logMessage
 }
 
-def transform(String inputXml) {
-    
-    // Parse the input XML
-    def parsedInputXml = new XmlParser().parseText(inputXml)
-    
-    // Extract the name and uuid from the input XML
-    def name = parsedInputXml.name.text()
-    def uuid = parsedInputXml.uuid.text()
- 
-    writeLog("@@@@ Inside trasformer.groovy -- VM Name: ${name} uuid: ${uuid}")
+def transform(String xml) {
+    // Parse the existing XML
+    def xmlParser = new XmlParser(false, false)
+    def domain = xmlParser.parseText(xml)
 
-    return inputXml
+    def vmName = domain.name.text()
+    writeLog("@@@@ Inside trasformer.groovy -- VM Name: ${vmName}")
+
+    return xml
+
+    // Example: Change domain type to 'kvm' from 'qemu'
+    domain.@type = 'kvm'
+
+    // Example: Add a custom disk element
+    def devices = domain.devices[0]
+    def newDisk = new Node(devices, 'disk')
+    newDisk.@type = 'file'
+    newDisk.@device = 'disk'
+    new Node(newDisk, 'driver', [name: 'qemu', type: 'qcow2'])
+    new Node(newDisk, 'source', [file: '/var/lib/libvirt/images/custom-disk.qcow2'])
+    new Node(newDisk, 'target', [dev: 'vdb', bus: 'virtio'])
+    new Node(newDisk, 'address', [type: 'pci', domain: '0x0000', bus: '0x00', slot: '0x05', function: '0x0'])
+
+    // Example: Change network interface model
+    def networkInterface = devices.interface[0]
+    networkInterface.model[0].@type = 'virtio'
+
+    // Serialize the modified XML back to a string
+    def xmlOutput = new StringWriter()
+    def printer = new XmlNodePrinter(new PrintWriter(xmlOutput))
+    printer.setPreserveWhitespace(true)
+    printer.print(domain)
+
+    return xmlOutput.toString()
 }
+    
+transform('<domain type="qemu"><name>systemvm-1</name></domain>')
