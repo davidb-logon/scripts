1efef6cd8d32667f813701b13b611f98fa71d89b
sefimlog <sefim@log-on.com>
2024-08-15 16:52:21 +0300

    added 44

---
+++ 44_create_systemvm.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#------------------------------------------------------------------------------
+# Licensed Materials (c) Copyright Log-On 2024, All Rights Reserved.
+#------------------------------------------------------------------------------
+# See usage for what this script does.
+# TODOs:
+
+# Source script libraries as needed.
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "$DIR/lib/common.sh"
+
+script_ended_ok=false
+trap 'cleanup' EXIT
+
+usage() {
+cat << EOF
+-------------------------------------------------------------------------------
+Configure the netwwork with Network Manager
+-------------------------------------------------------------------------------
+EOF
+script_ended_ok=true
+}
+
+main() {
+    init_vars "logon" "configure_network_wo_NM"
+    start_logging
+    check_if_root
+    install_systemve
+    script_ended_ok=true
+}
+
+install_systemve(){
+logMessage "Installing systemvm"
+do_cmd "virsh undefine debian10-1"
+if ! [ -f debian-10.8.0-s390x-netinst.iso ]; then
+  do_cmd "wget https://cdimage.debian.org/cdimage/archive/10.8.0/s390x/iso-cd/debian-10.8.0-s390x-netinst.iso"
+fi 
+if ! [ -f /data/primary/vm/images/debiaen108-1.qcow2 ]; then
+  do_cmd "qemu-img create -o preallocation=off -f qcow2 /data/primary/vm/images/debiaen108-1.qcow2 5242880000"
+fi
+#virt-install --name debian10-1 --memory 2048 --vcpus=2  --os-variant=debian10  --network network=default --graphics=none -v --disk path=/data/primary/vm/images/debiaen108-1.qcow2,size=6 --check disk_size=off --boot hd --location=/home/sefi/debian-10.8.0-s390x-netinst.iso
+virt-install --name debian10-1 --memory 2048 --vcpus=2  --os-variant=debian10  --network network=default --graphics=none -v --disk path=/data/primary/vm/images/debiaen108-1.qcow2,size=6 --check disk_size=off --boot hd --location=/home/sefi/debian-10.8.0-s390x-netinst.iso --extra-args ro 
+
+
+
+}
+init_vars() {
+    init_utils_vars $1 $2
+}
+
+
+main "$@"
