f8b7d2f03fc5b5ea212906e1d465ed0be4b97f9c
dbarta <david@odeca.net>
2024-08-18 11:53:56 +0300

    Added starting web server on repo befor mvn

--- 2_build_cloudstack_from_source.sh
+++ 2_build_cloudstack_from_source.sh
@@ -33,6 +33,7 @@ main() {
     install_maven.sh
     install_python_mkisof_mysql.sh
     install_java.sh
+    start_web_server_on_repo.sh
     cd "$CLOUDSTACK_DIR"
     mvn clean install -P developer,systemvm -DskipTests | tee "$LOGFILE"
     script_ended_ok=true
