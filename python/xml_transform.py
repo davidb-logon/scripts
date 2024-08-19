245ca2c695ef23e7fb9bfb805701f768f96f49e0
dbarta <david@odeca.net>
2024-08-19 12:42:56 +0300

    replace os node

--- python/xml_transform.py
+++ python/xml_transform.py
@@ -11,25 +11,53 @@ logging.basicConfig(
 )
 logger = logging.getLogger()
 
-def change_domain_type(xml_string):
+def update_xml(xml_string):
     # Parse the XML string
     root = ET.fromstring(xml_string)
     
     # Change the 'type' attribute of the 'domain' element to 'qemu'
     root.set('type', 'qemu')
     
+    # Find the 'devices' element
+    devices = root.find('devices')
+    
+    # Find and remove the 'graphics' element within 'devices'
+    graphics = devices.find('graphics')
+    if graphics is not None:
+        devices.remove(graphics)
+        
+    replace_os_node(root)
+    
     # Convert the modified XML tree back to a string
     modified_xml_string = ET.tostring(root, encoding='unicode')
     
     return modified_xml_string
 
+def replace_os_node(root):
+    # Find the 'os' element
+    os_node = root.find('os')
+    
+    # Replace the 'os' element with the new structure
+    if os_node is not None:
+        # Create a new 'os' element
+        new_os_node = ET.Element('os')
+        
+        # Create and append the 'type' element
+        type_element = ET.SubElement(new_os_node, 'type', arch='x86_64', machine='pc-i440fx-5.1')
+        type_element.text = 'hvm'
+        
+        # Create and append the 'boot' element
+        boot_element = ET.SubElement(new_os_node, 'boot', dev='hd')
+        
+        # Replace the old 'os' element with the new one
+        root.remove(os_node)
+        root.append(new_os_node)
+
 
 def manipulate_xml(xml_input):
     logger.info("@@@@ Starting XML manipulation")
-    # Perform your XML manipulation here
-    # For demonstration purposes, we'll just return the input string
     
-    modified_xml = change_domain_type(xml_input)
+    modified_xml = update_xml(xml_input)
     
     return modified_xml
 
