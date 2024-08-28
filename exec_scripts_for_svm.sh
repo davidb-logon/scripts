09cbafe858bb5b0365f69f43ff7663b79c97b3b4
dbarta <david@odeca.net>
2024-08-28 12:36:53 +0300

    script to execute in order scripts for svm template

---
+++ exec_scripts_for_svm.sh
@@ -0,0 +1,14 @@
+/home/sefi/scripts/apt_upgrade.sh
+/home/sefi/scripts/configure_grub.sh
+/home/sefi/scripts/configure_locale.sh
+/home/sefi/scripts/configure_networking.sh
+/home/sefi/scripts/configure_acpid.sh
+/home/sefi/scripts/install_systemvm_packages.sh
+/home/sefi/scripts/configure_conntrack.sh
+/home/sefi/scripts/authorized_keys.sh
+/home/sefi/scripts/configure_persistent_config.sh
+/home/sefi/scripts/configure_login.sh
+/home/sefi/cloud_scripts_shar_archive.sh
+/home/sefi/scripts/configure_systemvm_services.sh
+/home/sefi/scripts/cleanup.sh
+/home/sefi/scripts/finalize.sh
