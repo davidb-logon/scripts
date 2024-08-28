33c4625c728a8a12a5be06bce3ba51adb9eb5058
sefimlog <sefim@log-on.com>
2024-08-28 10:11:53 +0300

    deb_install - how to compile and install packer

--- deb_install.sh
+++ deb_install.sh
@@ -1,9 +1,12 @@
-#install packer - very long compilation time
+#install packer - very long compilation time - compile and install packer
 # cd /data
 # git clone https://github.com/hashicorp/packer.git
 # cd packer
 # make bin
-
+#   GOOS=linux GOARCH=s390x go build -o bin/packer .
+#   mv bin/packer /usr/local/bin/
+#   rm /sbin/packer
+#   ln -s /usr/local/bin/packer /sbin/packer
 
 virsh net-start default
 virt-install --name deb390-12-1 \
