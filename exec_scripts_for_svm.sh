61c4f334953acf040f44e3055c5573fecd36c088
sefimlog <sefim@log-on.com>
2024-09-02 13:23:29 +0300

    second half run

--- exec_scripts_for_svm.sh
+++ exec_scripts_for_svm.sh
@@ -1,12 +1,12 @@
 
 
-/home/sefi/scripts/apt_upgrade.sh  2>&1 | tee exec.log
-# /home/sefi/scripts/configure_grub.sh 2>&1 | tee exec.log
-/home/sefi/scripts/configure_locale.sh 2>&1 | tee exec.log
-/home/sefi/scripts/configure_networking.sh 2>&1 | tee exec.log
-/home/sefi/scripts/configure_acpid.sh 2>&1 | tee exec.log
-/home/sefi/scripts/install_systemvm_packages.sh 2>&1 | tee exec.log
-exit
+# /home/sefi/scripts/apt_upgrade.sh  2>&1 | tee exec.log
+# # /home/sefi/scripts/configure_grub.sh 2>&1 | tee exec.log
+# /home/sefi/scripts/configure_locale.sh 2>&1 | tee exec.log
+# /home/sefi/scripts/configure_networking.sh 2>&1 | tee exec.log
+# /home/sefi/scripts/configure_acpid.sh 2>&1 | tee exec.log
+# /home/sefi/scripts/install_systemvm_packages.sh 2>&1 | tee exec.log
+# exit
 /home/sefi/scripts/configure_conntrack.sh 2>&1 | tee exec.log
 /home/sefi/scripts/authorized_keys.sh 2>&1 | tee exec.log
 /home/sefi/scripts/configure_persistent_config.sh 2>&1 | tee exec.log
