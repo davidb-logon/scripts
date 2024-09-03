3ff529a1dcc4c5aece2d796d436285a4de3220b9
dbarta <david@odeca.net>
2024-09-03 15:15:24 +0300

    update svm to disable apparmor

--- 12_install_systemvm_for_kvm.sh
+++ 12_install_systemvm_for_kvm.sh
@@ -5,6 +5,6 @@
 set -x
 SCRIPT_PATH="/usr/share/cloudstack-common/scripts/storage/secondary/cloud-install-sys-tmplt"
 #SVM_PATH=http://download.cloudstack.org/systemvm/4.19/systemvmtemplate-4.19.1-kvm.qcow2.bz2
-SVM_PATH=http://localhost:8090/systemvmtemplate-deb12-s390x.qcow2.bz2
+SVM_PATH=http://localhost:8090/deb390-12-4-1.qcow2.bz2
 
 sudo $SCRIPT_PATH -m /data/mainframe_secondary -u $SVM_PATH -h kvm -F
