f37c829688a39fe10a79af9060a97fb83e7fe54f
sefimlog <sefim@log-on.com>
2024-09-09 12:36:19 +0300

    update 12 to stopp all starting machines

--- 12_install_systemvm_for_kvm.sh
+++ 12_install_systemvm_for_kvm.sh
@@ -24,6 +24,7 @@ main() {
     check_if_root
     extract_template_from_vm
     prepare_repo
+    verify_all_stopped
     register_template
     script_ended_ok=true
 }
@@ -61,4 +62,16 @@ register_template() {
     logMessage "End register template"
 }
 
+verify_all_stopped(){
+    vm_name=$(mysql -D cloud -se "SELECT name FROM vm_instance WHERE state='Starting';")
+
+    # Loop through each VM name and update its state to 'Stopped'
+    for vm in $vm_name; do
+        # Print the update statement and execute it
+        echo "Updating VM: $vm"
+        mysql -D cloud -e "UPDATE vm_instance SET state = 'Stopped' WHERE name = '$vm';"
+        virsh destroy $vm
+    done 
+
+}
 main $@
