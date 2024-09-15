9169b8f1fe9490bb54a0f122bfaffc3a0cb0656e
sefimlog <sefim@log-on.com>
2024-09-15 10:54:51 +0300

    test 12

--- 12_install_systemvm_for_kvm.sh
+++ 12_install_systemvm_for_kvm.sh
@@ -59,7 +59,7 @@ register_template() {
     SVM_PATH=${REPO_PATH}$(basename $FILE_PATH)
     logMessage "SVM PATH: $SVM_PATH"
     # do_cmd "$SCRIPT_PATH -m /data/mainframe_secondary -u ${SVM_PATH}.bz2 -h kvm -F"
-    do_cmd "$SCRIPT_PATH -m /data/mainframe_secondary -u ${SVM_PATH}?date=$(date +%Y%m%d)&version=1 -h kvm -F"
+    do_cmd "$SCRIPT_PATH -m /data/mainframe_secondary -u ${SVM_PATH}?date=$(date +%Y%m%d)\&version=1 -h kvm -F"
     do_cmd "systemctl restart cloudstack-management"
     logMessage "End register template"
 }
