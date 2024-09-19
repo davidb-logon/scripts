d73a53b9f267b8f6e5e9d47d94111fed8ce46059
sefimlog <sefim@log-on.com>
2024-09-19 16:09:36 +0300

    update git_updater.sh

--- util/git_updater.sh
+++ util/git_updater.sh
@@ -1,15 +1,17 @@
-cd /data/cloudstack
+for fldr in cloudstack scripts libvirt-java; do
+cd /data/$fldr
 pwd
 sudo git fetch
 sudo git pull
 git status
-cd /data/scripts
-pwd
-sudo git fetch
-sudo git pull
-git status
-cd /data/libvirt-java
-pwd
-sudo git fetch
-sudo git pull
-git status
+done
+# cd /data/scripts
+# pwd
+# sudo git fetch
+# sudo git pull
+# git status
+# cd /data/libvirt-java
+# pwd
+# sudo git fetch
+# sudo git pull
+# git status
