2b7e4513a322bb91a2f2dead0c1bae11a08eac7f
sefimlog <sefim@log-on.com>
2024-09-30 12:50:01 +0300

    update

--- systemvmtemplate/auto_install_deb_systemvm.sh
+++ systemvmtemplate/auto_install_deb_systemvm.sh
@@ -10,7 +10,7 @@ virt-install --name deb11-a \
     --memory 2048 --vcpus=2  --os-variant=debian11  \
     --network network=default --graphics=none -v \
     --disk path=/data/primary/vm/images/deb11-a.qcow2,size=6 \
-    --check disk_size=off --boot hd --location=/mnt/iso/debian/debian-11.11.0-s390x-netinst.iso \
+    --check disk_size=off --location=/mnt/iso/debian/debian-11.11.0-s390x-netinst.iso \
     --extra-args="auto=true priority=critical preseed/file=/preseed.cfg" \
     --initrd-inject=/data/scripts/systemvmtemplate/http/preseed_s390x.cfg \
 
