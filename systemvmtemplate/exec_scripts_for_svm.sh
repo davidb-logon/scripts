f82ab12124f9ad2f4c03986a41b6ecf33cb92ade
dbarta <david@odeca.net>
2024-09-30 16:51:53 +0300

    Added log messages between steps

--- systemvmtemplate/exec_scripts_for_svm.sh
+++ systemvmtemplate/exec_scripts_for_svm.sh
@@ -16,18 +16,45 @@ main() {
     start_logging
     check_if_root
     do_cmd "rm -f exec.log"
+    echo "@@@@@@@ Start executing scripts in order: $(date "+%m-%d %H:%M:%S")" | tee -a  exec.log
+
+    echo "@@@@@@@ Doing apt_upgrade.sh: $(date "+%m-%d %H:%M:%S")" | tee -a exec.log
     do_cmd "/home/sefi/scripts/apt_upgrade.sh  2>&1 | tee -a  exec.log"
+
+    echo "@@@@@@@ Doing configure_locale.sh: $(date "+%m-%d %H:%M:%S")" | tee -a exec.log
     do_cmd "/home/sefi/scripts/configure_locale.sh 2>&1 | tee -a exec.log"
+
+    echo "@@@@@@@ Doing configure_networking.sh: $(date "+%m-%d %H:%M:%S")" | tee -a exec.log
     do_cmd "/home/sefi/scripts/configure_networking.sh 2>&1 | tee -a exec.log"
+
+    echo "@@@@@@@ Doing configure_acpid.sh: $(date "+%m-%d %H:%M:%S")" | tee -a exec.log
     do_cmd "/home/sefi/scripts/configure_acpid.sh 2>&1 | tee -a exec.log"
+
+    echo "@@@@@@@ Doing install_systemvm_packages.sh: $(date "+%m-%d %H:%M:%S")" | tee -a exec.log
     do_cmd "/home/sefi/scripts/install_systemvm_packages.sh 2>&1 | tee -a exec.log"
+
+    echo "@@@@@@@ Doing configure_conntrack.sh: $(date "+%m-%d %H:%M:%S")" | tee -a exec.log
     do_cmd "/home/sefi/scripts/configure_conntrack.sh 2>&1 | tee -a exec.log"
+
+    echo "@@@@@@@ Doing authorized_keys.sh: $(date "+%m-%d %H:%M:%S")" | tee -a exec.log
     do_cmd "/home/sefi/scripts/authorized_keys.sh 2>&1 | tee -a exec.log"
+
+    echo "@@@@@@@ Doing configure_persistent_config.sh: $(date "+%m-%d %H:%M:%S")" | tee -a exec.log
     do_cmd "/home/sefi/scripts/configure_persistent_config.sh 2>&1 | tee -a exec.log"
+
+    echo "@@@@@@@ Doing configure_login.sh: $(date "+%m-%d %H:%M:%S")" | tee -a exec.log
     do_cmd "/home/sefi/scripts/configure_login.sh 2>&1 | tee -a exec.log"
+
+    echo "@@@@@@@ Doing cloud_scripts_shar_archive.sh: $(date "+%m-%d %H:%M:%S")" | tee -a exec.log
     do_cmd "/home/sefi/cloud_scripts_shar_archive.sh 2>&1 | tee -a exec.log"
+
+    echo "@@@@@@@ Doing configure_systemvm_services.sh: $(date "+%m-%d %H:%M:%S")" | tee -a exec.log
     do_cmd "/home/sefi/scripts/configure_systemvm_services.sh 2>&1 | tee -a exec.log"
+
+    echo "@@@@@@@ Doing cleanup.sh: $(date "+%m-%d %H:%M:%S")" | tee -a exec.log
     do_cmd "/home/sefi/scripts/cleanup.sh 2>&1 | tee -a exec.log"
+
+    echo "@@@@@@@ Doing finalize.sh: $(date "+%m-%d %H:%M:%S")" | tee -a exec.log
     do_cmd "/home/sefi/scripts/finalize.sh 2>&1 | tee -a exec.log"
 
     end_time=$(date +%s)
