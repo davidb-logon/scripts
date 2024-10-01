0f7f4912c13d9f62d7b3fe95b21315183c9b3cc2
dbarta <david@odeca.net>
2024-10-01 11:01:44 +0300

    added debug

--- systemvmtemplate/auto_install_deb_systemvm.sh
+++ systemvmtemplate/auto_install_deb_systemvm.sh
@@ -15,7 +15,7 @@ virt-install --name deb11-a \
     --disk path=/data/primary/vm/images/deb11-a.qcow2,size=6 \
     --check disk_size=off \
     --location=/mnt/iso/debian/debian-11.11.0-s390x-netinst.iso \
-    --extra-args="auto=true priority=critical preseed/file=/preseed_s390x.cfg DEBIAN_FRONTEND=noninteractive auto-install/enable=true" \
+    --extra-args="auto=true priority=critical preseed/file=/preseed_s390x.cfg DEBCONF_DEBUG=5 DEBIAN_FRONTEND=noninteractive auto-install/enable=true" \
     --initrd-inject="/data/scripts/systemvmtemplate/http/preseed_s390x.cfg"
 
 
