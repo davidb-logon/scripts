93d09dcbc6f93ac2fb9dc206d80a334d1d64062b
sefimlog <sefim@log-on.com>
2024-10-06 11:00:45 +0300

    update

--- systemvmtemplate/copy_scripts_to_systemvm_guest.sh
+++ systemvmtemplate/copy_scripts_to_systemvm_guest.sh
@@ -78,9 +78,9 @@ function wait_for_vm_ip() {
 }
 prepare_fresh_systemvm() {
     logMessage "First copy deb11-1 to systemvm guest deb11-systemvm "
-    do_cmd "virsh destroy deb11-1"
-    do_cmd "virsh destroy deb11-systemvm"
-    do_cmd "virsh undefine deb11-systemvm"
+    do_cmd "virsh destroy deb11-1" "" "INFO: Vm deb11-1 is stopped"
+    do_cmd "virsh destroy deb11-systemvm" "" "INFO: Vm deb11-systemvm is stopped"
+    do_cmd "virsh undefine deb11-systemvm"  "" "INFO: Vm deb11-systemvm is erased"
     do_cmd "virt-clone --original deb11-1  --name deb11-systemvm --auto-clone"
     do_cmd "virsh start deb11-systemvm"
     # Wait until systemvm is up
