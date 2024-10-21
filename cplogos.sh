104b09ddbedf46a36d2954feff788a143196c5fc
sefimlog <sefim@log-on.com>
2024-10-21 20:06:16 +0300

    update cplogos

--- cplogos.sh
+++ cplogos.sh
@@ -1,17 +1,20 @@
-[root@lr86xga ~]# cp -r /usr/share/cloudstack-management/webapp/assets /data/scripts/images/
-[root@lr86xga ~]# find /data/scripts/images/
-/data/scripts/images/
-/data/scripts/images/assets
-/data/scripts/images/assets/403.png
-/data/scripts/images/assets/success.png
-/data/scripts/images/assets/500.png
-/data/scripts/images/assets/banner.svg
-/data/scripts/images/assets/error.png
-/data/scripts/images/assets/google.svg
-/data/scripts/images/assets/github.svg
-/data/scripts/images/assets/404.png
-/data/scripts/images/assets/bg-what-is-cloudstack.png
-/data/scripts/images/assets/mini-logo.svg
-/data/scripts/images/assets/logo.svg
-/data/scripts/images/wavenextgen.svg
-/data/scripts/images/wavenextgen-withText2.svg
+# [root@lr86xga ~]# cp -r /usr/share/cloudstack-management/webapp/assets /data/scripts/images/
+# [root@lr86xga ~]# find /data/scripts/images/
+# /data/scripts/images/
+# /data/scripts/images/assets
+# /data/scripts/images/assets/403.png
+# /data/scripts/images/assets/success.png
+# /data/scripts/images/assets/500.png
+# /data/scripts/images/assets/banner.svg
+# /data/scripts/images/assets/error.png
+# /data/scripts/images/assets/google.svg
+# /data/scripts/images/assets/github.svg
+# /data/scripts/images/assets/404.png
+# /data/scripts/images/assets/bg-what-is-cloudstack.png
+# /data/scripts/images/assets/mini-logo.svg
+# /data/scripts/images/assets/logo.svg
+# /data/scripts/images/wavenextgen.svg
+# /data/scripts/images/wavenextgen-withText2.svg
+
+cp /data/scripts/images/wavenextgen.svg /usr/share/cloudstack-management/webapp/assets/mini-logo.svg
+cp /data/scripts/images/wavenextgen-withText2.svg /usr/share/cloudstack-management/webapp/assets/logo.svg
