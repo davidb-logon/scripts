95d77aef170a31cf4b0e82798ccc42f374f967e9
sefimlog <sefim@log-on.com>
2024-10-21 12:02:50 -0500

    cplogos.sh

---
+++ cplogos.sh
@@ -0,0 +1,17 @@
+[root@lr86xga ~]# cp -r /usr/share/cloudstack-management/webapp/assets /data/scripts/images/
+[root@lr86xga ~]# find /data/scripts/images/
+/data/scripts/images/
+/data/scripts/images/assets
+/data/scripts/images/assets/403.png
+/data/scripts/images/assets/success.png
+/data/scripts/images/assets/500.png
+/data/scripts/images/assets/banner.svg
+/data/scripts/images/assets/error.png
+/data/scripts/images/assets/google.svg
+/data/scripts/images/assets/github.svg
+/data/scripts/images/assets/404.png
+/data/scripts/images/assets/bg-what-is-cloudstack.png
+/data/scripts/images/assets/mini-logo.svg
+/data/scripts/images/assets/logo.svg
+/data/scripts/images/wavenextgen.svg
+/data/scripts/images/wavenextgen-withText2.svg
